6f10a4202f0c44d43c30f180d07aea9365b7fade
rayhan9999 <rayhanchowdhuri2023@gmail.com>
2023-03-26 20:05:02 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+
+      <LoadPost></LoadPost>
+
+      <District name="Nokhali" special="bivag"></District>
+      <District name="Bramonbaria" special="jodha akbar"></District>
+      <District name="Sumilla" special="moynamoti"></District>
+
+    </div>
+  );
+}
+
+function LoadPost() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
+  return (
+    <div>
+      <h1>Posts:{posts.length}</h1>
+      {
+
+        posts.map(post => <Post title={post.title} body={post.body}></Post>)
+      }
+
+    </div>
+  )
+
+}
+
+function Post(props) {
+  return (
+    <div style={{ backgroundColor: 'lightgray', margin: '20px', border: '4px solid salmon' }}>
+      <h2>Title:{props.title}</h2>
+      <p>Body:{props.body}</p>
+    </div>
+  )
+
+}
+
+
+const districtStyle = {
+  backgroundColor: 'lightblue',
+  margin: '20px',
+  borderRadius: '20px',
+  padding: '20px'
+}
+
+function District(props) {
+  const [power, setPower] = useState(1);
+
+
+  const boostPower = () => {
+    const newPower = power * 2;
+    setPower(newPower);
+  }
+
+
+  return (
+    <div style={districtStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty:{props.special}</p>
+      <h4>Power:{power}</h4>
+      <button onClick={boostPower}>Boost The Power</button>
+
+    </div>
+  )
+
+
+}
+
+export default App;
